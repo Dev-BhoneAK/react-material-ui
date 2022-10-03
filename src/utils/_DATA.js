f11c9c2c259b3ce72f8680f21a4826ebc761b7f1
BhoneAK <bhoneak@outlook.com>
2022-10-04 00:07:23 +0630

    feat:add Typography Component

---
+++ src/utils/_DATA.js
@@ -0,0 +1,29 @@
+let component_lists = [
+  {
+    id: 1,
+    name: "Buttons",
+    description: "All Material Buttons",
+  },
+  {
+    id: 2,
+    name: "Select",
+    description: "All Material Buttons",
+  },
+  {
+    id: 3,
+    name: "Text Field",
+    description: "All Material Buttons",
+  },
+  {
+    id: 4,
+    name: "Typography",
+    description: "All Material Buttons",
+  },
+  {
+    id: 5,
+    name: "Card",
+    description: "All Material Cards",
+  },
+];
+
+export default component_lists;
