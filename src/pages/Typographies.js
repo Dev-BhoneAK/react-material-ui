f425ee8b8d538a9147e0fc088a88ed4239123b26
BhoneAK <bhoneak@outlook.com>
2022-10-05 00:01:45 +0630

    feat:add pages folder

---
+++ src/pages/Typographies.js
@@ -0,0 +1,108 @@
+import * as React from "react";
+import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import SvgIcon from "@mui/material/SvgIcon";
+
+function LightBulbIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
+    </SvgIcon>
+  );
+}
+
+function ProTip() {
+  return (
+    <Typography sx={{ mt: 6, mb: 3 }} color="text.secondary">
+      <LightBulbIcon sx={{ mr: 1, verticalAlign: "middle" }} />
+      Pro tip: See more{" "}
+      <Link href="https://mui.com/getting-started/templates/">
+        templates
+      </Link>{" "}
+      on the MUI documentation.
+    </Typography>
+  );
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const Typographies = () => {
+  console.log("hi");
+  return (
+    <>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Create React App example
+          </Typography>
+          <Typography variant="h1" gutterBottom>
+            h1. Heading
+          </Typography>
+          <Typography variant="h2" gutterBottom>
+            h2. Heading
+          </Typography>
+          <Typography variant="h3" gutterBottom>
+            h3. Heading
+          </Typography>
+          <Typography variant="h4" gutterBottom>
+            h4. Heading
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            h5. Heading
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            h6. Heading
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Quos blanditiis tenetur
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Quos blanditiis tenetur
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
+            consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+            fugiat deleniti? Eum quasi quidem quibusdam.
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
+            consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+            fugiat deleniti? Eum quasi quidem quibusdam.
+          </Typography>
+          <Typography variant="button" display="block" gutterBottom>
+            button text
+          </Typography>
+          <Typography variant="caption" display="block" gutterBottom>
+            caption text
+          </Typography>
+          <Typography variant="overline" display="block" gutterBottom>
+            overline text
+          </Typography>
+
+          <ProTip />
+          <Copyright />
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+// return <Typographies />;
+export default <Typographies />;
