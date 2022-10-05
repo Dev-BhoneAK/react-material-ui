d7f62836c1c537a2d42b76ceb47e30af8d27c613
BhoneAK <bhoneak@outlook.com>
2022-10-05 20:03:09 +0630

    feat: add Cards

---
+++ src/pages/Cards.js
@@ -0,0 +1,76 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Container } from "@mui/system";
+import Stack from "@mui/material/Stack";
+import CardMedia from "@mui/material/CardMedia";
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+const Cards = () => {
+  return (
+    <Container sx={{ my: 4 }}>
+      <Stack spacing={3} direction="row" sx={{ my: 4 }}>
+        <Card sx={{ minWidth: "375px" }}>
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="div">
+              be{bull}nev{bull}o{bull}lent
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              adjective
+            </Typography>
+            <Typography variant="body2">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+        <Card>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Stack>
+    </Container>
+  );
+};
+
+export default <Cards />;
