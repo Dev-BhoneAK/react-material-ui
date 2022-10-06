2595ef53587a6d056ac2b28b6996117ff11c838b
BhoneAK <bhoneak@outlook.com>
2022-10-06 22:50:58 +0630

    fix: home.js

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,12 @@
 import * as React from "react";
 import HomeAppBar from "./components/app-bar/HomeAppBar.js";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import { Link as RouterLink } from "react-router-dom";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Divider from "@mui/material/Divider";
 import ListItemText from "@mui/material/ListItemText";
-import component_lists from "./utils/_DATA.js";
+import componentListData from "./utils/_DATA.js";
 
 const LinkBehavior = React.forwardRef((props, ref) => {
   console.log("link props ", props);
@@ -24,29 +21,19 @@ const LinkBehavior = React.forwardRef((props, ref) => {
   );
 });
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      {/* <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "} */}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 export default function Home() {
-  // component_lists.sort(
-  //   (a, b) => component_lists[a].name - component_lists[b].name
-  // );
+  const [componentLists, setComponentLists] = React.useState(componentListData);
+
   return (
     <>
-      <HomeAppBar />
+      <HomeAppBar
+        componentLists={componentLists}
+        setComponentLists={setComponentLists}
+      />
       <List
         sx={{ width: "100%", bgcolor: "background.paper", cursor: "pointer" }}
       >
-        {component_lists.map((component) => (
+        {componentLists.map((component) => (
           <Link component={LinkBehavior} to={`/components/${component.name}`}>
             <ListItem alignItems="flex-start">
               <ListItemText
@@ -58,27 +45,6 @@ export default function Home() {
           </Link>
         ))}
       </List>
-      {/* <Container maxWidth="lg">
-        <Box>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Create React App example
-          </Typography>
-          <ProTip />
-          <Copyright />
-        </Box>
-      </Container> */}
-      {/* <Box
-        sx={{
-          display: "flex",
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-          bgcolor: "inherit",
-          color: "text.primary",
-          borderRadius: 1,
-          p: 3,
-        }}
-      ></Box> */}
     </>
   );
 }
