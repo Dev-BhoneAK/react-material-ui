b77c68b933915732c98fbd86f5e74cc2ff64e799
BhoneAK <bhoneak@outlook.com>
2022-10-25 23:35:57 +0630

    feat:add Custom Tabs Data

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -3,26 +3,43 @@ let component_lists = [
     id: 1,
     name: "Buttons",
     description: "All Material Buttons",
+    favorite: true,
   },
   {
     id: 2,
     name: "Selects",
     description: "All Material Selects",
+    favorite: false,
   },
   {
     id: 3,
     name: "TextFields",
     description: "All Material TextFields",
+    favorite: true,
   },
   {
     id: 4,
     name: "Typographies",
     description: "All Material Typographies",
+    favorite: false,
   },
   {
     id: 5,
     name: "Cards",
     description: "All Material Cards",
+    favorite: true,
+  },
+  {
+    id: 6,
+    name: "Grids",
+    description: "All Material Grids",
+    favorite: false,
+  },
+  {
+    id: 7,
+    name: "CustomTabs",
+    description: "All Material Tabs",
+    favorite: false,
   },
 ];
 
