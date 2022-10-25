67756fcb3d045081e9111978bec0566aea347c36
BhoneAK <bhoneak@outlook.com>
2022-10-25 23:35:32 +0630

    feat:add Grids

---
+++ src/pages/Grids.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+const Grids = () => {
+  return (
+    <Container sx={{ my: 4 }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ my: 4 }}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 10 }}>
+          <Grid item xs={6}>
+            <Item>1</Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>2</Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>3</Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>4</Item>
+          </Grid>
+        </Grid>
+      </Box>
+      <Button variant="contained" color="success" sx={{ px: 2 * 10 }}>
+        Success
+      </Button>
+    </Container>
+  );
+};
+
+export default <Grids />;
